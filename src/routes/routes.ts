7cbad5fe9f9543be2646b9ae6c777988662d1fcd
IvanCastro19 <79170316+IvanCastro19@users.noreply.github.com>
2024-01-28 15:14:27 -0600

    new conditional for esim, get viertual sims if the condition is true in the params route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
-import { login, signIn, portHandler, getMSISDN, isMSISDNAvailable, pre_activate } from "../controllers/controllers";
+import { login, signIn, portHandler, getMSISDN, isMSISDNAvailable, pre_activate, imeiData } from "../controllers/controllers";
 import { validateUserData, validateLoginData, validateToken } from "../middlewares/validations";
 const routes = Router();
 
 routes.post("/login", validateLoginData, login)
     .post("/signin", validateUserData, signIn)
     .post("/port", validateToken, portHandler)
-    .post("/pre_activate/:msisdn", validateToken, pre_activate)
-    .post("/msisdn", validateToken, getMSISDN)
-    .get("/msisdn_count", validateToken, isMSISDNAvailable)
+    .post("/pre_activate/:msisdn/:esim", validateToken, pre_activate)
+    .post("/msisdn/:esim", validateToken, getMSISDN)
+    .get("/msisdn_count/:esim", validateToken, isMSISDNAvailable)
+    .get("/imeiData/:imei", imeiData)
     .post('/test_req', (req, res) => {
         console.log(req.body)
         return res.status(200).json({pass: true})
